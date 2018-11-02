16046cfcb7982857dd1ea48111b0e8c23c737988
Hongbo Li <hongbo.li.cs@gmail.com>
2018-11-01 21:52:58 -0700

    Comment changes.

--- cil/src/ext/crestBranch.ml
+++ cil/src/ext/crestBranch.ml
@@ -1,13 +1,14 @@
 (* Copyright (c) 2008, Jacob Burnim (jburnim@cs.berkeley.edu)
-*
-* This file is part of CREST, which is distributed under the revised
-* BSD license.  A copy of this license can be found in the file LICENSE.
-*
-* This program is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See LICENSE
-* for details.
-*)
+ * Copyright (c) 2018, Hongbo Li (hli035@cs.ucr.edu)
+ *
+ * This file is part of CREST, which is distributed under the revised
+ * BSD license.  A copy of this license can be found in the file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See LICENSE
+ * for details.
+ *)
 
 open Cil
 
